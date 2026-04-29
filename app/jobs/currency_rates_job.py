51556aad87e8c57a91828446830958e7e691eb7f
ydt <230103074@gmail.com>
2026-04-29 10:43:58 +0500

    add logging to currency job, fix created_at

--- app/jobs/currency_rates_job.py
+++ app/jobs/currency_rates_job.py
@@ -1,9 +1,15 @@
+import logging
+
 from app.db.session import SessionLocal
 from app.models.enums import Currency
 from app.services.currency.currency_service import CurrencyService
 
 async def update_currency_rates():
     async with SessionLocal() as db:
+        logging.info("Start updating currency rates job")
         rates = CurrencyService.fetch_rates_from_api(Currency.USD)
+        logging.info("Successfully fetched currency rates")
+
+        logging.info("Saving new currency rates")
         await CurrencyService.save_rates_to_db(db, rates, Currency.USD)
         await db.commit()
