fb720f8b236bfe4c2346cb3d22e4f70073964079
Dias.Izdibayev <dias.izdibayev@qcloudy.io>
2026-04-30 13:25:13 +0500

    fix preprocessv2

--- app/services/ocr/preprocess.py
+++ app/services/ocr/preprocess.py
@@ -1,26 +1,25 @@
 import asyncio
 import base64
-
-import cv2
+from PIL import Image
 import numpy as np
+import io
 
 from app.services.ocr.const import AppState
 
 
-async def preprocess_receipt(state: AppState, img_bytes) -> str:
-
-    img_array = np.frombuffer(img_bytes, np.uint8)
-    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+async def preprocess_receipt(state: AppState, img_bytes: bytes) -> str:
+    img = Image.open(io.BytesIO(img_bytes))
     if img is None:
         raise ValueError("Unable to decode uploaded image")
-
+    
     if state.yolo is not None:
-        results = await asyncio.to_thread(state.yolo.predict, img, verbose=False)
-        cropped = _crop_receipt(img, results)
-    else:
-        cropped = img
+        img_array = np.array(img)
+        results = await asyncio.to_thread(state.yolo.predict, img_array, verbose=False)
+        img = _crop_receipt(img, results)
 
-    b64 = base64.b64encode(cv2.imencode(".jpg", cropped)[1]).decode()
+    buffer = io.BytesIO()
+    # img.save(buffer, format="JPEG")
+    b64 = base64.b64encode(buffer.getvalue()).decode()
 
     return b64
 
